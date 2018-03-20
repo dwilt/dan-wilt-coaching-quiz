227523622c07799aa7ef4f29cbd9ec6478b16855
Dan Wilt <dwilt4rville@gmail.com>
2018-03-20 11:40:53 +0000

    put in cors into firebase code

--- firebaseFunctions/index.js
+++ firebaseFunctions/index.js
@@ -1,11 +1,17 @@
 const functions = require(`firebase-functions`);
 
+const cors = require(`cors`)({
+    origin: true
+});
+
 import { addResult } from "./firestore";
 
-exports.addResult = functions.https.onRequest(async (request, response) => {
-    await addResult(request.body);
+exports.addResult = functions.https.onRequest(async (req, res) => {
+    await addResult(req.body);
 
-    response.send({
-        stored: true
+    return cors(req, res, () => {
+        res.send({
+            stored: true
+        });
     });
 });
